a6deaaab2dd1bc29df0ca216fb16a4da48653e57
JacobJones6154 <68295733+JacobJones6154@users.noreply.github.com>
2020-09-13 17:08:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UsingProp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicles BasicVehicle = new Vehicles();
+            BasicVehicle.Movement();
+            Aircraft Helicopter = new Aircraft();
+            Helicopter.Movement();
+            Submersible Sub = new Submersible();
+            Sub.Movement();
+            Tanks Tank = new Tanks();
+            Tank.Movement();
+            Weapons BasicWeapon = new Weapons();
+            BasicWeapon.Shoot();
+            SmallCaliberWeapon AR15 = new SmallCaliberWeapon();
+            AR15.Shoot();
+            LargeCaliberWeapon MiniGun = new LargeCaliberWeapon();
+            MiniGun.Shoot();
+        }
+    }
+}
---
+++ Weapons.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingProp
+{
+    class Weapons
+    {
+        private string _name;
+        private double _caliber;
+        private string _type;
+
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
+        public double Caliber
+        {
+            get => this._caliber;
+            set => this._caliber = value;
+        }
+        public string Type
+        {
+            get => this._type;
+            set => this._type = value;
+        }
+        public virtual void Shoot()
+        {
+            Console.WriteLine("Butter Butter Jam Jam Butter Butter Jam Jam"); //??? This is something we had to yell during dry fire training in Combat training so everytime i think of shooting this comes to mindl
+        }
+    }
+    class SmallCaliberWeapon : Weapons
+    {
+        public override void Shoot()
+        {
+            Console.WriteLine("Pew Pew Pew Pew");
+        }
+    }
+    class LargeCaliberWeapon : Weapons
+    {
+        public override void Shoot()
+        {
+            Console.WriteLine("BANG BANG BANG");
+        }
+    }
+}
+
